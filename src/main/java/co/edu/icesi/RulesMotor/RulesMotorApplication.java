7cc526616090cdb6cd69b3468a0499ef9de93462
Juan Camilo <61602045+juanmiloz@users.noreply.github.com>
2022-12-08 23:52:50 -0500

    first commit

---
+++ src/main/java/co/edu/icesi/RulesMotor/RulesMotorApplication.java
@@ -0,0 +1,13 @@
+package co.edu.icesi.RulesMotor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RulesMotorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RulesMotorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/icesi/RulesMotor/RulesMotorApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.icesi.RulesMotor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulesMotorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
